468bfda748f9b63778e35ae236086c2e5f8eeba1
uyab <uyab.exe@gmail.com>
2015-11-16 13:38:52 +0700

    login with linked in

--- resources/views/auth/social.blade.php
+++ resources/views/auth/social.blade.php
@@ -1,13 +1,17 @@
-<div class="three ui buttons basic">
-    <a href="{{ url('auth/facebook/login') }}" class="ui button">
+<div class="four item ui menu basic labeled icon">
+    <a href="{{ url('auth/facebook/login') }}" class="item">
         <i class="facebook icon blue"></i>
         Facebook
     </a>
-    <a href="{{ url('auth/google/login') }}" class="ui button">
+    <a href="{{ url('auth/google/login') }}" class="item">
         <i class="google icon red"></i>
         Google
     </a>
-    <a href="{{ url('auth/github/login') }}" class="ui button">
+    <a href="{{ url('auth/linkedin/login') }}" class="item">
+        <i class="linkedin square icon blue"></i>
+        LinkedIn
+    </a>
+    <a href="{{ url('auth/github/login') }}" class="item">
         <i class="github icon black"></i>
         Github
     </a>
