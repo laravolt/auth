225ad504149e2cb9cb34ee9a916de3f63d49314e
uyab <uyab.exe@gmail.com>
2015-12-10 15:06:44 +0700

    fix class name

--- database/migrations/2015_08_28_000000_add_status_to_user.php
+++ database/migrations/2015_08_28_000000_add_status_to_user.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersActivationTable extends Migration
+class AddStatusToUser extends Migration
 {
     /**
      * Run the migrations.
