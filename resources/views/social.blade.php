98a880437c20e8ecdedb1a47a9fd436076ba983e
Bayu Hendra <uyab.exe@gmail.com>
2016-09-14 09:00:51 +0700

    social icon style

--- resources/views/social.blade.php
+++ resources/views/social.blade.php
@@ -2,7 +2,7 @@
 
     <div class="ui icon buttons basic">
         @foreach(config('laravolt.auth.services') as $service)
-            <a href="{{ route('auth::social.login', $service) }}" class="ui button">
+            <a href="{{ route('auth::social.login', $service) }}" class="ui icon button">
                 <i class="{{ $service }} icon"></i>
             </a>
         @endforeach
