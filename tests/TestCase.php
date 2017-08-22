842f4158a8c4816f459d8d112871996ac33ea432
uyab <uyab.exe@gmail.com>
2017-08-22 16:17:44 +0700

    clean up test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,7 +11,6 @@ abstract class TestCase extends BaseTestCase
 {
     protected function getDatabasePath()
     {
-        //return __DIR__.'/database/auth.sqlite';
         return ':memory:';
     }
 
@@ -48,35 +47,17 @@ abstract class TestCase extends BaseTestCase
         $app['config']->set('laravolt.auth.redirect.after_login', '/login-success');
         $app['config']->set('laravolt.auth.redirect.after_reset_password', '/reset-password-success');
         $app['config']->set('session.expire_on_close', false);
-
         //$app->make('Illuminate\Contracts\Http\Kernel')->pushMiddleware(\Illuminate\Session\Middleware\StartSession::class);
 
     }
 
     protected function setUpDatabase()
     {
-        //if (!file_exists($file = $this->getDatabasePath())) {
-        //    touch($file);
-        //} else {
-        //    $this->cleanDatabase();
-        //}
-
         $this->createUserTable();
-
-//        $this->loadLaravelMigrations();
 
         $this->loadMigrationsFrom(realpath(__DIR__.'/../database/migrations'));
 
-        $this->beforeApplicationDestroyed(function () {
-            $this->cleanDatabase();
-        });
-
         User::create(['name' => 'Andi', 'email' => 'andi@laravolt.com', 'password' => bcrypt('asdf1234')]);
-    }
-
-    protected function cleanDatabase()
-    {
-        //file_put_contents($this->getDatabasePath(), null);
     }
 
     protected function createUserTable()
@@ -86,7 +67,6 @@ abstract class TestCase extends BaseTestCase
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('status')->default('ACTIVE');
             $table->rememberToken();
             $table->timestamps();
         });
