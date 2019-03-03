5079aaec8e90fbca6248d73fdf4f30f9324e146e
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-03-03 21:29:54 +0700

    Change default layout

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'layout'       => 'ui::layouts.auth',
+    'layout'       => 'ui::layouts.public.full',
     'captcha'      => false,
     'identifier'   => 'email',
     'login' => [
