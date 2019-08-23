211399d6f49e8642fcc90852984d423e35996246
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-08-23 08:24:51 +0700

    allow to modify cas login response

--- src/Http/Controllers/Cas/Login.php
+++ src/Http/Controllers/Cas/Login.php
@@ -2,16 +2,23 @@
 
 namespace Laravolt\Auth\Http\Controllers\Cas;
 
+use Illuminate\Http\Request;
+
 class Login extends CasController
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         cas()->authenticate();
 
         $email = (config('cas.cas_masquerade')) ? cas()->user() : array_get(cas()->getAttributes(), 'email');
         $user = app(config('auth.providers.users.model'))->whereEmail($email)->first();
+        $loginService = app('laravolt.auth.login');
 
         if ($user && auth()->login($user)) {
+            if (method_exists($loginService, 'authenticated')) {
+                return $loginService->authenticated($request, $user);
+            }
+
             return redirect()->home();
         } else {
             return redirect()->route('auth::login')->withError(trans('auth::login_cas_failed'));
